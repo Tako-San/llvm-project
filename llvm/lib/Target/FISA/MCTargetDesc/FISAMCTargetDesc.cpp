1136015a6b7c7d93795c38e20f5dd2440ebed492
Tako-San <dxe13@mail.ru>
2024-03-13 23:38:52 +0300

    Add initial version of the FISA backend

---
+++ llvm/lib/Target/FISA/MCTargetDesc/FISAMCTargetDesc.cpp
@@ -0,0 +1,4 @@
+#include "FISAMCTargetDesc.h"
+
+// We need to define this function for linking succeed
+extern "C" void LLVMInitializeFISATargetMC() {}
---
+++ llvm/lib/Target/FISA/TargetInfo/FISATargetInfo.h
@@ -0,0 +1,12 @@
+#ifndef __LLVM_LIB_TARGET_FISA_TARGETINFO_FISATARGETINFO_HH__
+#define __LLVM_LIB_TARGET_FISA_TARGETINFO_FISATARGETINFO_HH__
+
+namespace llvm {
+
+class Target;
+
+Target &getTheFISATarget();
+
+} // end namespace llvm
+
+#endif // __LLVM_LIB_TARGET_FISA_TARGETINFO_FISATARGETINFO_HH__
